de5a464f834e570a5ad40cb80fca44fbc1419e74
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-03-28 13:15:23 +0600

    images card fetching

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 const Product = (props) => {
-    const {img,name,seller,ratings,price} = props.product;
+    const { img, name, seller, ratings, price} = props.product;
     const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
-            <img src="{img}" alt="" />
+            <img src={ img } alt="" />
             <div className='product-info'>
                 <h6 className='product-name'>{name}</h6>
                 <p>Price: ${price}</p>
